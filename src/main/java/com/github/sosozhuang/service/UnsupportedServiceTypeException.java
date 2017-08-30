649944f297998692ce390be13a2892b68dc0127b
sosozhuang <sosozhuang@163.com>
2017-08-30 14:26:30 +0800

    Add UnsupportedServiceTypeException, throw exception when message service type not supported.

---
+++ src/main/java/com/github/sosozhuang/service/UnsupportedServiceTypeException.java
@@ -0,0 +1,12 @@
+package com.github.sosozhuang.service;
+
+public class UnsupportedServiceTypeException extends Exception {
+    private static final long serialVersionUID = 1967889586001169615L;
+
+    public UnsupportedServiceTypeException() {
+        super();
+    }
+    public UnsupportedServiceTypeException(String s) {
+        super(s);
+    }
+}
