2f1100eb7a3166ce2a7513266ea55462b3745f3b
sosozhuang <sosozhuang@163.com>
2017-08-30 22:45:59 +0800

    Rename class name, add serial UID.

---
+++ src/main/java/com/github/sosozhuang/service/UnsupportedException.java
@@ -0,0 +1,12 @@
+package com.github.sosozhuang.service;
+
+public class UnsupportedException extends Exception {
+    private static final long serialVersionUID = 1967889586001169615L;
+
+    public UnsupportedException() {
+        super();
+    }
+    public UnsupportedException(String s) {
+        super(s);
+    }
+}
