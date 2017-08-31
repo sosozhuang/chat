236e23aeaf07ec9fa9a03601877cbc0c3e2aec81
sosozhuang <sosozhuang@163.com>
2017-08-31 17:07:10 +0800

    Add project entry.

---
+++ src/main/java/com/github/sosozhuang/ChatMain.java
@@ -0,0 +1,52 @@
+package com.github.sosozhuang;
+
+import com.github.sosozhuang.conf.Configuration;
+import com.github.sosozhuang.conf.ServerConfiguration;
+import com.github.sosozhuang.service.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+public final class ChatMain {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ChatMain.class);
+
+    public static void main(String[] args) {
+        try {
+            Configuration config;
+            String configPath = System.getProperty("config");
+            if (configPath == null) {
+                config = new Configuration();
+            } else {
+                config = new Configuration(new File(configPath));
+            }
+
+            ServerConfiguration serverConf = new ServerConfiguration(config);
+            CloseableMetaService metaService = ServiceFactory.createMetaService(config);
+            CloseableMessageService messageService = ServiceFactory.createMessageService(config);
+            ChatServer server = new ChatServer(serverConf, metaService, messageService);
+            server.init();
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                server.stop();
+                try {
+                    metaService.close();
+                } catch (IOException e) {
+                    LOGGER.error("Close meta service error.", e);
+                }
+                try {
+                    messageService.close();
+                } catch (IOException e) {
+                    LOGGER.error("Close message service error.", e);
+                }
+                LOGGER.info("Chat service stopped.");
+                synchronized (ChatMain.class) {
+                    ChatMain.class.notify();
+                }
+            }));
+            server.start();
+        } catch (Exception e) {
+            LOGGER.error("Unable to start chat service.", e);
+        }
+    }
+}
