ce492b730463baad122e981c726c4232add8e20e
sosozhuang <sosozhuang@163.com>
2017-09-02 21:19:15 +0800

    Add IdleStateTrigger.

---
+++ src/main/java/com/github/sosozhuang/IdleStateTrigger.java
@@ -0,0 +1,20 @@
+package com.github.sosozhuang;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+
+public class IdleStateTrigger extends ChannelInboundHandlerAdapter {
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent) {
+            IdleState state = ((IdleStateEvent) evt).state();
+            if (state == IdleState.READER_IDLE) {
+                throw new Exception("Idle event triggered exception.");
+            }
+        } else {
+            super.userEventTriggered(ctx, evt);
+        }
+    }
+}
