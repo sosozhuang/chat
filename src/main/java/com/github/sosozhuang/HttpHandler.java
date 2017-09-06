a732f687c7cb341c6dca565e4f136357f16560fa
sosozhuang <sosozhuang@163.com>
2017-09-06 18:14:57 +0800

    Modify handler, only handle access control.

--- src/main/java/com/github/sosozhuang/HttpHandler.java
+++ src/main/java/com/github/sosozhuang/HttpHandler.java
@@ -4,10 +4,14 @@ import com.github.sosozhuang.protobuf.Chat;
 import com.github.sosozhuang.service.MetaService;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.*;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
-import io.netty.handler.ssl.SslHandler;
-import io.netty.util.AttributeKey;
+import io.netty.handler.codec.http.cookie.Cookie;
+import io.netty.handler.codec.http.cookie.DefaultCookie;
+import io.netty.handler.codec.http.cookie.ServerCookieEncoder;
 import io.netty.util.CharsetUtil;
 import io.netty.util.internal.StringUtil;
 import org.slf4j.Logger;
@@ -16,7 +20,10 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 import static io.netty.handler.codec.http.HttpMethod.GET;
 import static io.netty.handler.codec.http.HttpMethod.POST;
@@ -26,12 +33,19 @@ import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 public class HttpHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
     private static final Logger LOGGER = LoggerFactory.getLogger(HttpHandler.class);
     private static final Set<String> STATIC_FILES = new HashSet<>(Arrays.asList("html", "jpg", "png", "js", "map"));
-    static final AttributeKey GROUP = AttributeKey.valueOf("group");
-    static final AttributeKey USER = AttributeKey.valueOf("user");
+    //    static final AttributeKey GROUP = AttributeKey.valueOf("group");
+//    static final AttributeKey USER = AttributeKey.valueOf("user");
     private MetaService metaService;
+    private SecureRandom random;
 
     public HttpHandler(MetaService metaService) {
         this.metaService = metaService;
+        try {
+            random = SecureRandom.getInstance("SHA1PRNG");
+        } catch (NoSuchAlgorithmException e) {
+            random = new SecureRandom();
+        }
+        random.setSeed(System.currentTimeMillis());
     }
 
 
@@ -63,27 +77,30 @@ public class HttpHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
         String uri = request.uri();
         String p = new URI(uri).getPath();
         if ("/".equals(p)) {
-            sendRedirectResponse(ctx, "/index.html");
+            sendRedirectResponse(ctx, request, "/index.html", null);
             return;
         }
 
-        if ("/index.html".equals(p)) {
+        if ("/chat".equals(p)) {
             Map<String, List<String>> params = (new QueryStringDecoder(uri)).parameters();
             String user = getQueryParam(params, "user");
             if (StringUtil.isNullOrEmpty(user)) {
-                ctx.fireChannelRead(request.retainedDuplicate());
+                sendHttpResponse(ctx, request, new DefaultFullHttpResponse(HTTP_1_1, UNAUTHORIZED));
+//                ctx.fireChannelRead(request.retainedDuplicate());
                 return;
             }
 
             String groupID = getQueryParam(params, "group");
             if (StringUtil.isNullOrEmpty(groupID)) {
-                ctx.fireChannelRead(request.retainedDuplicate());
+                sendHttpResponse(ctx, request, new DefaultFullHttpResponse(HTTP_1_1, UNAUTHORIZED));
+//                ctx.fireChannelRead(request.retainedDuplicate());
                 return;
             }
 
             String token = getQueryParam(params, "token");
             if (StringUtil.isNullOrEmpty(token)) {
-                ctx.fireChannelRead(request.retainedDuplicate());
+                sendHttpResponse(ctx, request, new DefaultFullHttpResponse(HTTP_1_1, UNAUTHORIZED));
+//                ctx.fireChannelRead(request.retainedDuplicate());
                 return;
             }
 
@@ -111,9 +128,20 @@ public class HttpHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
                 return;
             }
 
-            ctx.channel().attr(GROUP).set(groupID);
-            ctx.channel().attr(USER).set(user);
-            ctx.fireChannelRead(request.retainedDuplicate());
+//            ctx.channel().attr(GROUP).set(groupID);
+//            ctx.channel().attr(USER).set(user);
+            byte[] bytes = new byte[12];
+            random.nextBytes(bytes);
+            String t = Base64.getEncoder().encodeToString(bytes);
+            Cookie cookie = new DefaultCookie("access-token", t);
+            cookie.setMaxAge(TimeUnit.SECONDS.toSeconds(8));
+
+            Chat.Access.Builder builder = Chat.Access.newBuilder();
+            builder.setGroupId(groupID);
+            builder.setUser(user);
+            metaService.setExpireToken(t.getBytes(), builder.build(), 8);
+            sendRedirectResponse(ctx, request, "/index.html", cookie);
+//            ctx.fireChannelRead(request.retainedDuplicate());
             return;
         }
 
@@ -193,13 +221,13 @@ public class HttpHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
 
     }
 
-    private static String getWebSocketLocation(ChannelPipeline pipeline, HttpRequest req, String path) {
-        String protocol = "ws";
-        if (pipeline.get(SslHandler.class) != null) {
-            protocol = "wss";
-        }
-        return protocol + "://" + req.headers().get(HttpHeaderNames.HOST) + path;
-    }
+//    private static String getWebSocketLocation(ChannelPipeline pipeline, HttpRequest req, String path) {
+//        String protocol = "ws";
+//        if (pipeline.get(SslHandler.class) != null) {
+//            protocol = "wss";
+//        }
+//        return protocol + "://" + req.headers().get(HttpHeaderNames.HOST) + path;
+//    }
 
     private static void sendHttpResponse(ChannelHandlerContext ctx, FullHttpRequest request, FullHttpResponse response) {
         ChannelFuture f = ctx.channel().writeAndFlush(response);
@@ -208,10 +236,13 @@ public class HttpHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
         }
     }
 
-    private static void sendRedirectResponse(ChannelHandlerContext ctx, String location) {
+    private static void sendRedirectResponse(ChannelHandlerContext ctx, FullHttpRequest request, String location, Cookie cookie) {
         DefaultFullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, TEMPORARY_REDIRECT);
         response.headers().set(HttpHeaderNames.LOCATION, location);
-        ctx.channel().writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+        if (cookie != null) {
+            response.headers().set(HttpHeaderNames.SET_COOKIE, ServerCookieEncoder.STRICT.encode(cookie));
+        }
+        ChannelFuture f = ctx.channel().writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
     }
 
     @Override
