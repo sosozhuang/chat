f077bdd0f6e096c0feac4c616efec5e75f0f92fa
sosozhuang <sosozhuang@163.com>
2017-09-07 16:57:32 +0800

    Remove throw Exception from receive().

--- src/main/java/com/github/sosozhuang/service/Receiver.java
+++ src/main/java/com/github/sosozhuang/service/Receiver.java
@@ -3,6 +3,6 @@ package com.github.sosozhuang.service;
 import com.github.sosozhuang.protobuf.Chat;
 
 public interface Receiver {
-    public <K, V> Iterable<MessageRecord<K, V>> receive() throws Exception;
-    public <K, V> Iterable<MessageRecord<K, V>> receive(String user, Chat.Group group, long timestamp) throws Exception;
+    public <K, V> Iterable<MessageRecord<K, V>> receive();
+    public <K, V> Iterable<MessageRecord<K, V>> receive(String user, Chat.Group group, long timestamp);
 }
