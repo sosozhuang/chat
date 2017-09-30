87bdc7ace9aea6b36d79cc8c4c737bbe37a687bc
sosozhuang <sosozhuang@163.com>
2017-09-30 16:21:32 +0800

    Add ActiveMQConfig.

---
+++ src/main/java/com/github/sosozhuang/conf/ActiveMQConfig.java
@@ -0,0 +1,8 @@
+package com.github.sosozhuang.conf;
+
+public class ActiveMQConfig {
+    private Configuration config;
+    public ActiveMQConfig(Configuration config) {
+        this.config = config;
+    }
+}
