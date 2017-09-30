3d0761362107be927a973828838b0df38cd2acc1
sosozhuang <sosozhuang@163.com>
2017-09-30 19:17:46 +0800

    Add activemq support.

--- src/main/java/com/github/sosozhuang/conf/ActiveMQConfig.java
+++ src/main/java/com/github/sosozhuang/conf/ActiveMQConfig.java
@@ -1,8 +1,48 @@
 package com.github.sosozhuang.conf;
 
+import org.apache.activemq.ActiveMQConnection;
+
+import javax.jms.ConnectionFactory;
+
 public class ActiveMQConfig {
     private Configuration config;
     public ActiveMQConfig(Configuration config) {
         this.config = config;
     }
+
+    public String getUserName() {
+        return getUserName(ActiveMQConnection.DEFAULT_USER);
+    }
+    public String getUserName(String defaultValue) {
+        return config.getString("activemq.conn.username", defaultValue);
+    }
+
+    public String getPassword() {
+        return getPassword(ActiveMQConnection.DEFAULT_PASSWORD);
+    }
+    public String getPassword(String defaultValue) {
+        return config.getString("activemq.conn.password", defaultValue);
+    }
+
+    public String getBrokerURL() {
+        return getBrokerURL(ActiveMQConnection.DEFAULT_BROKER_URL);
+    }
+    public String getBrokerURL(String defaultValue) {
+        return config.getString("activemq.conn.broker_url", defaultValue);
+    }
+
+    public String getTopicPattern() {
+        return getTopicPattern(null);
+    }
+    public String getTopicPattern(String defaultValue) {
+        return config.getString("activemq.topic.pattern", defaultValue);
+    }
+
+    public int getTopicCount() {
+        return getTopicCount(0);
+    }
+    public int getTopicCount(int defaultValue) {
+        return config.getInteger("activemq.topic.count", defaultValue);
+    }
+
 }
--- src/main/java/com/github/sosozhuang/service/ActiveMQMessageService.java
+++ src/main/java/com/github/sosozhuang/service/ActiveMQMessageService.java
@@ -2,18 +2,33 @@ package com.github.sosozhuang.service;
 
 import com.github.sosozhuang.conf.ActiveMQConfig;
 import com.github.sosozhuang.protobuf.Chat;
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQTopic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.jms.*;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.concurrent.Future;
 
 public class ActiveMQMessageService implements CloseableMessageService {
     private static final Logger LOGGER = LoggerFactory.getLogger(ActiveMQMessageService.class);
     private ActiveMQConfig config;
+    private ConnectionFactory connectionFactory;
+    private Connection connection;
+    private Session session;
 
-    public ActiveMQMessageService(ActiveMQConfig config) {
+    public ActiveMQMessageService(ActiveMQConfig config) throws JMSException {
         this.config = config;
+        this.connectionFactory = new ActiveMQConnectionFactory(config.getBrokerURL());
+        connection = connectionFactory.createConnection(config.getUserName(), config.getPassword());
+        connection.start();
+        session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+        destination = session.createTopic(config.getTopicPattern() + "*");
+        messageProducer = session.createProducer(destination);
+        TopicSubscriber subscriber = session.createDurableSubscriber(session.createTopic(config.getTopicPattern() + "*"), "xx");
     }
 
     @Override
@@ -33,6 +48,24 @@ public class ActiveMQMessageService implements CloseableMessageService {
 
     @Override
     public void close() throws IOException {
-
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (JMSException e) {
+                LOGGER.error("Close activemq connection error.", e);
+            } finally {
+                connection = null;
+            }
+        }
+        if (session != null) {
+            try {
+                session.commit();
+                session.close();
+            } catch (JMSException e) {
+                LOGGER.error("Close activemq session error.", e);
+            } finally {
+                session = null;
+            }
+        }
     }
 }
